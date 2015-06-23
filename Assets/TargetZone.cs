ed9c5ad37ac23a6c62558a95c95e41991ac16210
thesambassador <thesambassador@gmail.com>
2015-06-23 16:08:29 -0700

    added obstacles, circular target zone

--- Assets/TargetZone.cs
+++ Assets/TargetZone.cs
@@ -27,7 +27,16 @@ public class TargetZone : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            ZoneCompleted();
+        }
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            Rect r = new Rect(6.25f, 13.75f, 7.5f, 7.5f);
+            Vector2 v = new Vector2(10.60691f, 6.227509f);
+            Debug.Log(DistancePointToRectangle(v, r));
+        }
         SetColor(Color.Lerp(startColor, Color.green, curZoneDuration / zoneDuration));
 	}
 
@@ -57,6 +66,46 @@ public class TargetZone : MonoBehaviour
     void ZoneCompleted()
     {
         curZoneDuration = 0;
-        TestRunner.ZoneCompleted();
+        float minValid = -18;
+        float maxValid = 18;
+
+        
+
+        float x = 0;
+        float y = 0;
+
+        bool intersectingRectangle = true;
+
+        while(intersectingRectangle){
+
+            x = Random.Range(minValid, maxValid);
+            y = Random.Range(minValid, maxValid);
+            Vector2 point = new Vector2(x, y);
+
+            intersectingRectangle = !isValidPlacement(point);
+        }
+
+        this.transform.position = new Vector3(x, .01f, y);
+        //TestRunner.ZoneCompleted();
+    }
+
+    bool isValidPlacement(Vector2 point){
+        Rect[] rectsToAvoid = new Rect[4];
+        rectsToAvoid[0] = new Rect(-13.75f, 13.75f, 7.5f, 7.5f);
+        rectsToAvoid[1] = new Rect(6.25f, 13.75f, 7.5f, 7.5f);
+        rectsToAvoid[2] = new Rect(-13.75f, -6.25f, 7.5f, 7.5f);
+        rectsToAvoid[3] = new Rect(6.25f, -6.25f, 7.5f, 7.5f);
+
+        foreach(Rect r in rectsToAvoid){
+            if(DistancePointToRectangle(point, r) < 2) return false;
+        }
+        return true;
+    }
+
+    public static float DistancePointToRectangle(Vector2 p, Rect r)
+    {
+        var cx = Mathf.max(Mathf.min(p.x, r.left + r.width), r.left);
+        var cy = Mathf.max(Mathf.min(p.y, r.top + r.height), r.top);
+        return Math.sqrt((px - cx) * (px - cx) + (py - cy) * (py - cy));
     }
 }
